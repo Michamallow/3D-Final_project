ea2ae10b0f49d4c3ff98bb455839388d8fb40175
Michamallow <77727902+Michamallow@users.noreply.github.com>
2023-12-02 14:36:40 +0100

    Simple planet rendering

---
+++ Assets/Scripts/ShapeSettings.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ShapeSettings", menuName = "3D-Final_project/ShapeSettings", order = 0)]
+public class ShapeSettings : ScriptableObject 
+{
+	public float planetRadius = 1;
+}
