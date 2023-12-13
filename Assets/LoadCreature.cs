e16c4dd1c982a39ef5f09c30b433d3676c4c7b42
Bejo62192 <130389920+Bejo62192@users.noreply.github.com>
2023-12-13 16:53:36 +0100

    Begining menu

---
+++ Assets/LoadCreature.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadCreature : MonoBehaviour
+{
+    public int id;
+    public string nom;
+    private static LoadCreature load;
+
+    private void Awake()
+    {
+        if (load == null)
+        {
+
+            DontDestroyOnLoad(this.gameObject);
+            load = this;
+        }
+        else if (load != this)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Setpath(int id, string nom)
+    {
+        this.nom = nom;
+        this.id = id;
+    }
+}
---
+++ Assets/Piston.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Piston : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    private SliderJoint2D joint;
+    private float accumulatedTime = 0.0f;
+
+    public float amplitude = 0.2f;
+    public float offset = 0.0f;
+    public float frequence = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        joint = GetComponent<SliderJoint2D>();
+
+        JointMotor2D motor2d = joint.motor;
+        motor2d.motorSpeed = 1;
+        joint.motor = motor2d;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        accumulatedTime += Time.deltaTime;
+
+        JointMotor2D motor2d = joint.motor;
+        float speed = amplitude * Mathf.Sin(offset + (frequence * accumulatedTime));
+        Debug.Log(speed);
+        motor2d.motorSpeed = speed;
+        joint.motor = motor2d;
+    }
+}
