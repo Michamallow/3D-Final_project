8e8c901f2481cfbc835ab56d37299ca5354d7644
Bejo62192 <130389920+Bejo62192@users.noreply.github.com>
2023-12-13 15:46:32 +0100

    Main menu
    add drag and drop before add earth

---
+++ Assets/AudioMix.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class AudioMix : MonoBehaviour
+{
+
+    public Slider sliderVolume;
+    public Slider sliderMusique;
+    private int i = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SettingMenu setting = UnityEngine.Object.FindObjectOfType<SettingMenu>();
+        Debug.Log(setting.GetMusique());
+        sliderMusique.value = setting.GetMusique();
+        sliderVolume.value = setting.GetVolume();
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        while (i < 2)
+        {
+            Scene scene = SceneManager.GetActiveScene();
+            if (scene.name == "Niveau1")
+            {
+                SettingMenu setting = UnityEngine.Object.FindObjectOfType<SettingMenu>();
+                setting.ChangeMusic();
+            }
+            i++;
+        }
+    }
+
+    public void SetMusique(float musique)
+    {
+        SettingMenu setting = UnityEngine.Object.FindObjectOfType<SettingMenu>();
+        setting.SetMusique(musique);
+    }
+
+    public void SetVolume(float volume)
+    {
+        SettingMenu setting = UnityEngine.Object.FindObjectOfType<SettingMenu>();
+        setting.SetVolume(volume);
+    }
+
+
+}
---
+++ Assets/MainMenu.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject PanelPrincipal;
+    public GameObject PanelOption;
+    public bool panelOnOff;
+    
+
+    public void LOAD_SCENE()
+    {
+        SceneManager.LoadScene("Levelselect");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Exit");
+    }
+
+    public void OPTION()
+    {
+        PanelOption.SetActive(true);
+        PanelPrincipal.SetActive(false);
+        panelOnOff = true;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Confined;
+        Time.timeScale = 0;
+    }
+
+    public void BUTTON_BACK()
+    {
+        PanelOption.SetActive(false);
+        PanelPrincipal.SetActive(true);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            panelOnOff = !panelOnOff;
+            
+            if (panelOnOff)
+             {
+                 PanelPrincipal.SetActive(false);
+                 Cursor.visible = false;
+                 Cursor.lockState = CursorLockMode.Locked;
+                 Time.timeScale = 1;
+             }
+             else
+             {
+                PanelOption.SetActive(false);
+                PanelPrincipal.SetActive(true);
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.Confined;
+                Time.timeScale = 0;
+             }
+        }
+
+    }
+}
