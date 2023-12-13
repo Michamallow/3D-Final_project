805f357920ea7fc1c2aa9c3e6a4a92cd3ccc54da
Bejo62192 <130389920+Bejo62192@users.noreply.github.com>
2023-12-13 16:53:36 +0100

    Main Menu and Options

---
+++ Assets/SettingMenu.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class SettingMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    private static SettingMenu setting;
+    public AudioClip sonJeu;
+
+    private void Awake()
+    {
+        if (setting == null)
+        {
+
+            DontDestroyOnLoad(this.gameObject);
+            setting = this;
+        }
+        else if (setting != this)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("fx", volume);
+    }
+
+    public void SetMusique(float musique)
+    {
+        audioMixer.SetFloat("musique", musique);
+    }
+
+    public float GetVolume()
+    {
+        float vol;
+        bool res = audioMixer.GetFloat("fx", out vol);
+
+        if (res)
+        {
+            return vol;
+        }
+        else
+        {
+            return 0f;
+        }
+    }
+    public float GetMusique()
+    {
+        float mus;
+        bool res = audioMixer.GetFloat("musique", out mus);
+
+        if (res)
+        {
+            return mus;
+        }
+        else
+        {
+            return 0f;
+        }
+    }
+
+    public void ChangeMusic()
+    {
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.clip = sonJeu;
+        audio.Play();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
