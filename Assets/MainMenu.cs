905a60457f12900527ea7af0aca5a3a2567c25cc
Théodore Prévot <prevottheodore@gmail.com>
2024-01-17 01:13:59 +0100

    ft: redirect to earth scene

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -20,7 +20,7 @@ public class MainMenu : MonoBehaviour
 
     public void LOAD_SCENE()
     {
-        SceneManager.LoadScene("Flag");
+        SceneManager.LoadScene("EarthScene");
     }
 
     public void ExitGame()
